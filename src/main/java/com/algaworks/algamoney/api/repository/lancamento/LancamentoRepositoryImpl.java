3d370f1d7aa6664b663367918557b3b286a17a89
Ailton Luiz <ailtonatn@gmail.com>
2021-03-27 12:36:45 +0100

    Implementando metados

--- src/main/java/com/algaworks/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/algaworks/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
@@ -2,9 +2,16 @@ package com.algaworks.algamoney.api.repository.lancamento;
 
 import com.algaworks.algamoney.api.model.Lancamento;
 import com.algaworks.algamoney.api.repository.filter.LancamentoFilter;
+import org.springframework.util.ObjectUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
@@ -15,6 +22,37 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 
     @Override
     public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter) {
-        return null;
+
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Lancamento> criteriaQuery = builder.createQuery(Lancamento.class);
+        Root<Lancamento> root = criteriaQuery.from(Lancamento.class);
+
+
+        Predicate[] predicates = criarRestricoes(lancamentoFilter, builder, root);
+        criteriaQuery.where(predicates);
+
+        TypedQuery<Lancamento> query = entityManager.createQuery(criteriaQuery);
+
+        return query.getResultList();
+    }
+
+    private Predicate[] criarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder, Root<Lancamento> root) {
+
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (!ObjectUtils.isEmpty(lancamentoFilter.getDescricao())) {
+
+        }
+
+        if (lancamentoFilter.getDataVencimentoDe() != null) {
+
+        }
+
+        if (lancamentoFilter.getDataVencimentoAte() != null) {
+
+        }
+
+
+        return predicates.toArray(new Predicate[predicates.size()]);
     }
 }
